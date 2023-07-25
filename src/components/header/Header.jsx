c20683f3051b72d295db17e8edd7f35a719bce43
Mithoonns2001 <mithoonns@gmail.com>
2023-07-25 16:24:56 +0530

    updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA/>
         <HeaderSocials/>
 
-        <div className="me">
+        <div className="me header-buttons">
           <img src={ME} alt="me" />
         </div>
         
